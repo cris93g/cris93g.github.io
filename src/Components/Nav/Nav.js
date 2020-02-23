7c092ed0b908937bdcd2115e202b2895f0dfa026
cris93g <37388115+cris93g@users.noreply.github.com>
2020-02-23 16:28:03 -0600

    setup

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Main_nav} from "./Nav.styled"
+class Nav extends Component {
+	render() {
+		return <div><Main_nav>
+            <div>
+                <p>logosdasdad</p>
+            </div>
+            </Main_nav></div>;
+	}
+}
+
+export default Nav;
---
+++ src/Components/Nav/Nav.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const Main_nav= styled.div `
+    width:100vw;
+    height:10vh;
+    background-color:black;
+    margin:0;
+    padding:0;
+`
